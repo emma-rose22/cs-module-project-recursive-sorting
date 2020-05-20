3fa18eee7b68ba1f0aaacb0475fe533cb3a1786c
Emma <strainwatch3@gmail.com>
2020-05-20 15:18:41 -0400

    merge done

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,50 +1,88 @@
-# TO-DO: complete the helper function below to merge 2 sorted arrays
-def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+# # TO-DO: complete the helper function below to merge 2 sorted arrays
+# #def merge(arrA, arrB):
+#     elements = len(arrA) + len(arrB)
+#     merged_arr = [0] * elements
 
-    #merge two sorted arrays
+#     #merge two sorted arrays
 
-    i = 0
-    j = 0
-    k = 0
+#     i = 0
+#     j = 0
+#     k = 0
 
-    while i < len(arrA) and j < len(arrB):
-        #check if first element is smaller in first array than second
-        if arrA[i] < arrB[j]:
-            merged_arr[k] = arrA[i]
-            k +=1
-            i += 1
-        #if it isn't, append the value from B instead
-        else:
-            merged_arr[k] = arrB[j]
-            k += 1
-            j += 1
+#     while i < len(arrA) and j < len(arrB):
+#         #check if first element is smaller in first array than second
+#         if arrA[i] < arrB[j]:
+#             merged_arr[k] = arrA[i]
+#             k +=1
+#             i += 1
+#         #if it isn't, append the value from B instead
+#         else:
+#             merged_arr[k] = arrB[j]
+#             k += 1
+#             j += 1
 
-    #until we are done (incrementing i to know if we went through 
-    #the whole list)
-    #elements that weren't sorted above are already 
-    #less than b, but bigger than the a values already sorted
-    #so we can add all of them 
-    while i < len(arrA):
-        merged_arr[k] = arrA[i]
-        k += 1
-        i += i
+#     #until we are done (incrementing i to know if we went through 
+#     #the whole list)
+#     #elements that weren't sorted above are already 
+#     #less than b, but bigger than the a values already sorted
+#     #so we can add all of them 
+#     while i < len(arrA):
+#         merged_arr[k] = arrA[i]
+#         k += 1
+#         i += i
 
-    #same idea here, all the elements w/ merge conflicts
-    #have been taken care of, leaving only elements safe to 
-    #append in order, no conflict from A
-    while j < len(arrB):
-        merged_arr[k] = arrB[j]
-        k += 1
-        j += 1
+#     #same idea here, all the elements w/ merge conflicts
+#     #have been taken care of, leaving only elements safe to 
+#     #append in order, no conflict from A
+#     while j < len(arrB):
+#         merged_arr[k] = arrB[j]
+#         k += 1
+#         j += 1
 
-    return merged_arr
+#     return merged_arr
 
 # a = [1, 2, 3]
 # b = [4, 5, 6]
 
 # print(merge(a, b))
+
+#LESSON CODE 
+
+def merge(A, B):
+    # init the combined list of A and B
+    #combined = [0 for _ in range(len(A) + len(B))]
+    combined = []
+
+    #init the two pointers that start at each list
+
+    a = 0
+    b = 0
+
+    while a < len(A) and b < len(B):
+        # compare the elements that a and b point at
+        if A[a] < B[b]:
+            combined.append(A[a])
+            a+=1
+
+        else:
+            combined.append(B[b])
+            b+=1
+
+    #at this point, we have finished traversing one of the lists
+    # we don't know which one, as the while breaks if either finishes
+
+    #we need to add the remaining elements to the combined list
+
+    #only one of these loops will be used
+    while a < len(A):
+        combined.append(A[a])
+        a +=1
+
+    while b < len(B):
+        combined.append(B[b])
+        b+=1
+
+    return combined
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
@@ -53,7 +91,7 @@ def merge_sort(arr):
     arrA = []
     arrB = []
     #can't sort a list of one
-    if len(arr) < 2:
+    if len(arr) > 1:
 
         #get the middle of the list
         half_len = len(arr)//2
@@ -73,13 +111,33 @@ def merge_sort(arr):
 
     
     #second, merge the lists together using function
-    arr = merge(arrA, arrB)
+        arr = merge(arrA, arrB)
+
+
+    return arr
+
+arr = [5, 3, 8, 6, 9, 2, 3, 10]
+
+#print(merge_sort(arr))
+
+#LESSON CODE 
+
+def merge_sort(arr):
+    #break the array down recursively
+    #base case: when the lists have length of one
+    # merge them back up
+
+    if len(arr) > 1:
+        left = merge_sort(arr[:len(arr)//2])
+        right = merge_sort(arr[len(arr)//2:])
+        arr = merge(left, right)
 
     return arr
 
 arr = [5, 3, 8, 6, 9, 2, 3, 10]
 
 print(merge_sort(arr))
+
 
 
 # implement an in-place merge sort algorithm
